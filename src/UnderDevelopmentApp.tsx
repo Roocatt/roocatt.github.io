f4a780fcdc65a394498153cd77200bc67e767e79
Roos Catling-Tate <roos@catling-tate.net>
2026-01-21 18:19:21 -0500

    Add dev page

---
+++ src/UnderDevelopmentApp.tsx
@@ -0,0 +1,11 @@
+
+
+export default function UnderDevelopmentApp() {
+    return (
+        <div>
+            <h1>Under Development</h1>
+            <p>This page is currently under development. Please check back later.</p>
+            <p>In the mean time, you can see what projects Roos is working on on her <a href="https://github.com/Roocatt">GitHub profile.</a></p>
+        </div>
+    );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import UnderDevelopmentApp from './UnderDevelopmentApp.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+/* Temporary condition to just render the actual page in development. */
+if (import.meta.env.DEV) {
+    createRoot(document.getElementById('root')!).render(
+        <StrictMode>
+            <App/>
+        </StrictMode>,
+    )
+} else {
+        createRoot(document.getElementById('root')!).render(
+        <StrictMode>
+            <UnderDevelopmentApp />
+        </StrictMode>,
+    )
+}
