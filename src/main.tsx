3423f1af613bd6170027ab5c26a07f2ee289a0bf
Roos Catling-Tate <roos@catling-tate.net>
2026-02-20 09:38:32 -0500

    Don't treat tablets as mobile

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { isMobile } from 'react-device-detect';
+import { isMobileOnly } from 'react-device-detect';
 import '98.css'
 import './css/index.css'
 import App from './App.tsx'
@@ -8,7 +8,7 @@ import MobileApp from "./MobileApp.tsx";
 /*import UnderDevelopmentApp from "./UnderDevelopmentApp.tsx";*/
 
 /*if (import.meta.env.DEV) {*/
-if (isMobile) {
+if (isMobileOnly) {
     await import('./css/mobile.css');
     createRoot(document.getElementById('root')!).render(
         <StrictMode>
